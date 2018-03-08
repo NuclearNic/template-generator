a630fc1218541ecfac172045ac64afebed5f69c5
nuclearnic <nicholas.erasmus@jumo.world>
2018-03-08 15:19:29 +0200

    add pdfkit gem

--- app/controllers/pdf_templates_controller.rb
+++ app/controllers/pdf_templates_controller.rb
@@ -1,5 +1,7 @@
 # it does what it says man
 class PdfTemplatesController < ApplicationController
+  before_action :get_template_elements
+
   def index
     @pdf_templates = PdfTemplate.all
   end
@@ -9,7 +11,13 @@ class PdfTemplatesController < ApplicationController
   end
 
   def pdfkit_generate
-
+    html = render_to_string(:action => 'pdfkit/content.html.erb', :layout => false)
+    pdf = PDFKit.new(html)
+    # send_data pdf.to_pdf,
+    send_data pdf.to_pdf,
+                type: 'application/pdf',
+                disposition: 'inline' # either "inline" or "attachment"
+    # send_data(pdf.to_pdf)
   end
 
   def prawn_generate
@@ -21,14 +29,9 @@ class PdfTemplatesController < ApplicationController
   end
 
   def wicked_generate
-    pdf_template = PdfTemplate.find(params[:pdf_template_id])
-    @header = pdf_template.header
-    @content = add_date(pdf_template.content)
-    @footer = pdf_template.footer ? add_page_number(pdf_template.footer) : ''
-
     render  pdf: 'filename.pdf',
             template: 'pdf_templates/wicked/content.html.erb',#, :show_as_html => true
-            title: pdf_template.title,
+            title: @pdf_template.title,
             page_size: 'A4',
             margin:  { top: 0, bottom: 15, left: 0, right: 0 },
             show_as_html: false, dpi: '300', save_only: false,
@@ -40,6 +43,13 @@ class PdfTemplatesController < ApplicationController
   end
 
   private
+
+  def get_template_elements
+    @pdf_template = PdfTemplate.find(params[:pdf_template_id])
+    @header = @pdf_template.header
+    @content = add_date(@pdf_template.content)
+    @footer = @pdf_template.footer ? add_page_number(@pdf_template.footer) : ''
+  end
 
   def add_page_number(footer)
     footer
